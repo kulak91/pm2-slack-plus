ea2a5629b62cb48225079e0ff5cfcb0421c5fcff
Dmytro Kulakov <kulakovdmitr@gmail.com>
2022-10-24 19:01:41 +0300

    git push

--- slack-receiver.js
+++ slack-receiver.js
@@ -206,7 +206,7 @@ app.message('info_app', async ({ message, client, say, payload }) => {
 
     const filePath = '/root/app/logs/strapi-out.log';
     const data = await fs.readFile(filePath);
-    const fileName = path.basename(filePath);
+    const fileName = 'Strapi-Log';
     await client.files.upload({ file: data, channels: message.channel, filename: fileName, token: configFile["SLACK_BOT_TOKEN"] });
   } catch (err) {
     console.log(err);
